d9ab8786d9c3f5ba44cb7c8adfa3f65316f7244a
Nathan Beaumont <nathan.beaumont@perkinelmer.com>
2023-05-06 19:03:49 -0400

    add print statements to LoadHOG.py

--- LoadHOGs.py
+++ LoadHOGs.py
@@ -8,3 +8,6 @@ with open(database_path / 'labels.pkl', "rb") as fp:
     label_list = pickle.load(fp)
 
 hog_matrix = np.load(database_path / 'hog_matrix.npy')
+
+print(hog_matrix.shape)
+print(len(label_list))
